32481b9f109b3787b922fe6dceef189af30e8a64
charles <your-gitcommitcg@gmail.com>
2020-07-04 08:54:31 -0400

    updated image file tpyes that can be uploaded by user

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -9,6 +9,9 @@ class Listing < ApplicationRecord
   has_one_attached :thumbnail
   has_many_attached :images
   has_rich_text :description
+  validates :title, presence: true
+  validates :description, presence: true
+  validate :image_type
   acts_as_votable
 
   def resized_thumbnail
@@ -17,5 +20,17 @@ class Listing < ApplicationRecord
 
   def manyimage input
     return self.images[input].variant(resize: '350x250!').processed
+  end
+
+  private
+  def image_type
+    if images.attached? == false
+      errors.add(:images, "are missing!")
+    end
+    images.each do |image|
+      if !image.content_type.in?(%('image/jpeg image/jpg image/png'))
+        errors.add(:images, 'needs to be a JPEG, JPG or PNG')
+      end
+    end
   end
 end
