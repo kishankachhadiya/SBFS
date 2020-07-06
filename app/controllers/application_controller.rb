b2f2f31b874445530d141459a9e096de03f41560
charles <your-gitcommitcg@gmail.com>
2020-07-06 07:22:35 -0400

    fixed actiontext and alerts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :set_featured
   helper_method :is_admin!
+  before_action :set_current_user
   def set_featured
     # Grab makes that are featured to true and show them by newest
     @featured = Listing.where(featured: true).order('created_at ASC')
@@ -8,8 +9,14 @@ class ApplicationController < ActionController::Base
 
   def is_admin!
     unless current_user&.admin
-      redirect_to root_path
+      redirect_to new_user_registration_path
     end
   end
+
+  def set_current_user
+    #create a new use if there is no logged in user
+    #or you could probably load a default user
+    current_user = User.new unless user_signed_in?  
+  end 
 
 end
