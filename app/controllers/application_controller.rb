7bf6fd33fe6c3da0488f5df9f230bf787f5e4df8
charles <your-gitcommitcg@gmail.com>
2020-07-06 10:16:28 -0400

    herokkuuu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_featured
   helper_method :is_admin!
-  before_action :set_current_user
   before_action :authenticate_user!
   def set_featured
     # Grab makes that are featured to true and show them by newest
@@ -11,11 +10,5 @@ class ApplicationController < ActionController::Base
   def is_admin!
     current_user&.admin
   end
-
-  def set_current_user
-    #create a new use if there is no logged in user
-    #or you could probably load a default user
-    current_user = User.new unless user_signed_in?  
-  end 
 
 end
